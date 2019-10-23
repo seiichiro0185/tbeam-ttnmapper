ed5a9db214525243061f626aa9bcf1ef7cbdae6a
Stefan Brand <seiichiro@seiichiro0185.org>
2019-10-23 08:09:51 +0200

    Disable Rate Adaptation

--- src/main.cpp
+++ src/main.cpp
@@ -214,7 +214,7 @@ void setup() {
   LMIC_reset();
   LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
 
-  #ifdef CFG_eu868
+  // Setup EU Channels
   LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
   LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
   LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
@@ -224,10 +224,11 @@ void setup() {
   LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
   LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
   LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-  #endif
-
+  
   // TTN uses SF9 for its RX2 window.
   LMIC.dn2Dr = DR_SF9;
+  // Disable Data Rate Adaptation, for Mapping we want static SF7
+  LMIC_setAdrMode(0);
   // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
   LMIC_setDrTxpow(DR_SF7,14); 
   do_send(&sendjob);
